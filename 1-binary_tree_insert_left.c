dd087622a64d822b607831e20a8180a09b8ec3dc
NO6B <abdel_azrhar@hotmail.com>
2025-08-05 17:08:08 +0200

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	return (NULL);
 	}
 	if (parent->left != NULL)
-	{    
+	{
 	parent->left->parent = new_node;
 	}
 	parent->left = new_node;
