b85843a99cbab2e764bc61a2e21bee0ad2f82112
Abdel <abdel_azrhar@hotmail.com>
2025-08-05 17:02:09 +0200

    fix betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,19 +1,25 @@
 #include "binary_trees.h"
 #include <stdlib.h>
+/**
+ * binary_tree_insert_left - inserts a node as the left child
+ * @parent: pointer to parent node
+ * @value: value to store in new node
+ * Return: pointer to new node, or NULL on failure
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
-    if (parent->left != NULL)
-    {
-        
-        parent->left->parent = new_node;
-    }
-    parent->left = new_node;
+	binary_tree_t *new_node;
 
-    return (new_node);
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+	return (NULL);
+	}
+	if (parent->left != NULL)
+	{    
+	parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+
+	return (new_node);
 }
--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,13 +1,19 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_size - measures size of a binary tree
+ * @tree: pointer to root node
+ * Return: total number of nodes
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t i = 0;
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    i++;
-    i += binary_tree_size(tree->left);
-    i += binary_tree_size(tree->right);
-    return (i);
+	size_t i = 0;
+	
+	if (tree == NULL)
+	{
+	return (0);
+	}
+	i++;
+	i += binary_tree_size(tree->left);
+	i += binary_tree_size(tree->right);
+	return (i);
 }
