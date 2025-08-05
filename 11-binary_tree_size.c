b25aef44757d265ffa9ecafa511e3d54c5b8e080
Abdel <abdel_azrhar@hotmail.com>
2025-08-05 12:00:59 +0200

    push task

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    size_t i = 0;
+    if (tree == NULL)
+    {
+        return (0);
+    }
+    i++;
+    i += binary_tree_size(tree->left);
+    i += binary_tree_size(tree->right);
+    return (i);
+}
